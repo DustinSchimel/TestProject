d8fa75e387d17f9987b06a541e2ccff27f1f65ab
Dustin Schimel <dustin.schimel@gmail.com>
2018-05-01 12:42:08 -0600

    Added background animation

--- TestProject/Controller/AirGame.cs
+++ TestProject/Controller/AirGame.cs
@@ -19,6 +19,12 @@ namespace AirGame
 		SpriteBatch spriteBatch;
 		// Represents the player 
 		private Player player;
+		// Image used to display the static background
+		private Texture2D mainBackground;
+
+		// Parallaxing Layers
+		private ParallaxingBackground bgLayer1;
+		private ParallaxingBackground bgLayer2;
 
 		// Keyboard states used to determine key presses
 		private KeyboardState currentKeyboardState;
@@ -53,6 +59,9 @@ namespace AirGame
 			// Set a constant player move speed
 			playerMoveSpeed = 8.0f;
 
+			bgLayer1 = new ParallaxingBackground();
+			bgLayer2 = new ParallaxingBackground();
+
 			base.Initialize();
 		}
 
@@ -74,6 +83,11 @@ namespace AirGame
 			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
 			player.Initialize(playerAnimation, playerPosition);
 
+			// Load the parallaxing background
+			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
+			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
+
+			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
 		}
 
 		/// <summary>
@@ -105,6 +119,10 @@ namespace AirGame
 			UpdatePlayer(gameTime);
 
 			base.Update(gameTime);
+
+			// Update the parallaxing background
+			bgLayer1.Update();
+			bgLayer2.Update();
 		}
 
 		/// <summary>
@@ -118,6 +136,13 @@ namespace AirGame
 			//TODO: Add your drawing code here
 			// Start drawing 
 			spriteBatch.Begin(); 
+
+			spriteBatch.Draw(mainBackground, Vector2.Zero, Color.White);
+
+			// Draw the moving background
+			bgLayer1.Draw(spriteBatch);
+			bgLayer2.Draw(spriteBatch);
+
 			// Draw the Player 
 			player.Draw(spriteBatch); 
 			// Stop drawing 
